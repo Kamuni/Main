80ff15c06e9a041c2c775895ee838028451e3c15
Kamuni <mjas04@mail.ru>
2021-08-16 15:06:49 +0300

    Space Counter Task + Tests

---
+++ Space Counter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Space_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString = Console.ReadLine(); 
+            var usingClass  = new Space_count();
+            int answer = usingClass.spacesInString(userString);
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Space Counter/Space count.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Counter
+{
+    public class Space_count
+    {
+        public int spacesInString(string input)
+        {
+            string str = input;
+            int length = str.Length;
+            int count = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+
+}
+
